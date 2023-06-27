50f8283f57f0cedcc77ddaff59fe1b31430da69e
Armin Hossein Zadeh <Armin.Fullstack@gmail.com>
2023-06-27 15:44:48 +0330

    feat: rendered Pizzas

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,113 @@
+const pizzaData = [
+  {
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoName: "./public/assets/images/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: "./public/assets/images/margherita.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 12,
+    photoName: "./public/assets/images/spinaci.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photoName: "./public/assets/images/funghi.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Salamino",
+    ingredients: "Tomato, mozarella, and pepperoni",
+    price: 15,
+    photoName: "./public/assets/images/salamino.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photoName: "./public/assets/images/prosciutto.jpg",
+    soldOut: false,
+  },
+];
+interface PizzaProps {
+  pizzaObj: {
+    photoName: string;
+    name: string;
+    ingredients: string;
+    price: number;
+    soldOut: boolean,
+  }
+}
 const App: React.FC = () => {
   return (
     <div className="container">
-    <Header/>
-    <Menu/>
-    <Footer/>
+      <Header />
+      <Menu />
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
 function Header(): JSX.Element {
   return (
     <header className="header">
       <h1>Fast React Pizza Co.</h1>
     </header>
-  )
+  );
 }
 
 function Menu(): JSX.Element {
-  return(
+  return (
     <main className="menu">
       <h2>Our menu</h2>
-      <Pizza />
+      <ul className="pizzas">
+        {pizzaData.map((pizza) => {
+          return <Pizza pizzaObj={pizza} />;
+        })}
+      </ul>
     </main>
-  )
+  );
+}
+
+function Pizza(props: PizzaProps): JSX.Element {
+  return (
+    <li className="pizza">
+      <figure>
+        <img src={props.pizzaObj.photoName} alt="photo" />
+      </figure>
+      <div>
+        <h3>{props.pizzaObj.name}</h3>
+        <p>{props.pizzaObj.ingredients}</p>
+        <span>{props.pizzaObj.price}</span>
+      </div>
+    </li>
+  );
 }
 
 function Footer(): JSX.Element {
-  const hour = new Date().getHours()
-  const openHour = 20
-  const closeHour = 22
-  const isOpen = hour >= openHour && hour <= closeHour
+  const hour = new Date().getHours();
+  const openHour = 20;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
   console.log(isOpen);
-  return(
+  return (
     <footer className="footer">
       <p>We are curently open! {new Date().toLocaleTimeString()}</p>
     </footer>
-  )
+  );
 }
 
-function Pizza(): JSX.Element {
-  return(
-    <figure>
-      <img src="assets/images/salamino.jpg" alt="photo" />
-    </figure>
-  )
-}
 export default App;
